57c52a5b714bba48a4ba5534f9276f1ebcb4d241
Aiden <quang.tran290699@gmail.com>
2026-05-19 13:14:56 +0700

    Add @vercel/speed-insights dependency and integrate component in layout

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -4,6 +4,7 @@ import '@/styles/globals.css'
 import CustomFooter from '@/components/custom-footer'
 import CustomHeader from '@/components/custom-header'
 import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { BackToTop } from '@/components/back-to-top'
 import { LocaleSync } from '@/components/locale-sync'
 import { Metadata } from 'next'
@@ -39,12 +40,7 @@ type Props = {
 export default async function LocaleLayout({ children, params }: Props) {
   const { lang } = await params
   return (
-    <html
-      lang={lang}
-      dir="ltr"
-      suppressHydrationWarning
-      className={bodyFont.className}
-    >
+    <html lang={lang} dir="ltr" suppressHydrationWarning className={bodyFont.className}>
       <Head backgroundColor={{ dark: '#1a1a1a', light: '#ffffff' }} />
       <body className="min-h-screen">
         <Layout>
@@ -57,6 +53,7 @@ export default async function LocaleLayout({ children, params }: Props) {
         <BackToTop />
         <LocaleSync />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
